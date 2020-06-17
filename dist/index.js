55d26acd801dd1ec7d7356b2d03cc30c7a82459e
Lukas Gjetting <lukasgjetting@gmail.com>
2020-06-17 19:57:11 +0200

    Try using the checkout action

--- dist/index.js
+++ dist/index.js
@@ -5917,24 +5917,33 @@ const core = __webpack_require__(580);
 const main = async () => {
 	const octokit = github.getOctokit(process.env.INPUT_REPOTOKEN);
 
+	core.startGroup('Environment variables');
+	console.log(JSON.stringify(process.env, null, 2));
+	core.endGroup();
+
 	const owner = process.env.GITHUB_REPOSITORY_OWNER;
 	const repoName = process.env.GITHUB_REPOSITORY.replace(`${owner}/`, '');
 
 	console.log({ owner, repoName });
 
-	const repo = await octokit.repos.get({
-		owner,
-		repo: repoName,
-	});
+	try {
+		const repo = await octokit.repos.get({
+			owner,
+			repo: repoName,
+		});
 
-	console.log(JSON.stringify(repo, null, 2));
+		console.log(JSON.stringify(repo, null, 2));
 
-	const wikiRepo = await octokit.repos.get({
-		owner,
-		repo: `${repoName}.wiki`,
-	});
+		const wikiRepo = await octokit.repos.get({
+			owner,
+			repo: `${repoName}.wiki`,
+		});
 
-	console.log(JSON.stringify(wikiRepo, null, 2));
+		console.log(JSON.stringify(wikiRepo, null, 2));
+	} catch (e) {
+		console.log('An error has occurred :-O');
+		console.error(e);
+	}
 };
 
 main();
--- src/index.js
+++ src/index.js
@@ -4,24 +4,33 @@ const core = require('@actions/core');
 const main = async () => {
 	const octokit = github.getOctokit(process.env.INPUT_REPOTOKEN);
 
+	core.startGroup('Environment variables');
+	console.log(JSON.stringify(process.env, null, 2));
+	core.endGroup();
+
 	const owner = process.env.GITHUB_REPOSITORY_OWNER;
 	const repoName = process.env.GITHUB_REPOSITORY.replace(`${owner}/`, '');
 
 	console.log({ owner, repoName });
 
-	const repo = await octokit.repos.get({
-		owner,
-		repo: repoName,
-	});
+	try {
+		const repo = await octokit.repos.get({
+			owner,
+			repo: repoName,
+		});
 
-	console.log(JSON.stringify(repo, null, 2));
+		console.log(JSON.stringify(repo, null, 2));
 
-	const wikiRepo = await octokit.repos.get({
-		owner,
-		repo: `${repoName}.wiki`,
-	});
+		const wikiRepo = await octokit.repos.get({
+			owner,
+			repo: `${repoName}.wiki`,
+		});
 
-	console.log(JSON.stringify(wikiRepo, null, 2));
+		console.log(JSON.stringify(wikiRepo, null, 2));
+	} catch (e) {
+		console.log('An error has occurred :-O');
+		console.error(e);
+	}
 };
 
 main();
