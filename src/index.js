3d003a9250a966b5da484efd2c5e294c5e8a40f6
Lukas Gjetting <lukasgjetting@gmail.com>
2020-06-17 19:41:46 +0200

    List repositories

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
-console.log(
-	'Hello world!',
-	process.env.ACTIONS_RUNTIME_TOKEN,
-	process.env.INPUT_REPOTOKEN,
-	process.env.ACTIONS_RUNTIME_TOKEN === process.env.INPUT_REPOTOKEN,
-	JSON.stringify(process.env, null, 2),
-);
+const main = async () => {
+	const octokit = github.getOctokit(process.env.INPUT_REPOTOKEN);
 
-const octokit = github.getOctokit(process.env.INPUT_REPOTOKEN);
+	console.log((await octokit.orgs.list()));
+	console.log(JSON.stringify(octokit, null, 2));
+};
 
-console.log(octokit);
-console.log(JSON.stringify(octokit, null, 2));
+main();
