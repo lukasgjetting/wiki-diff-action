822a528c45263fd96204c9e516df173e3ab35aae
Lukas Gjetting <lukasgjetting@gmail.com>
2020-06-17 19:06:55 +0200

    Add some debug logging

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+console.log('Hello world!', github.token, JSON.stringify(github, null, 2));
+
+const octokit = github.getOctokit(github.token);
+
+console.log(octokit);
+console.log(JSON.stringify(octokit, null, 2));
