babbd5a4826e522636fd415eac62bb448025af67
Lukas Gjetting <lukasgjetting@gmail.com>
2020-06-17 19:47:20 +0200

    Get the runner repository

--- src/index.js
+++ src/index.js
@@ -4,8 +4,24 @@ const core = require('@actions/core');
 const main = async () => {
 	const octokit = github.getOctokit(process.env.INPUT_REPOTOKEN);
 
-	console.log((await octokit.orgs.list()));
-	console.log(JSON.stringify(octokit, null, 2));
+	const owner = process.env.GITHUB_REPOSITORY_OWNER;
+	const repoName = process.env.GITHUB_REPOSITORY.replace(`${owner}/`, '');
+
+	console.log({ owner, repoName });
+
+	const repo = await octokit.repos.get({
+		owner,
+		repo: repoName,
+	});
+
+	console.log(JSON.stringify(repo, null, 2));
+
+	const wikiRepo = await octokit.repos.get({
+		owner,
+		repo: `${repoName}.wiki`,
+	});
+
+	console.log(JSON.stringify(wikiRepo, null, 2));
 };
 
 main();
